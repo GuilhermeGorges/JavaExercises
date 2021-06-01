0ac8c0130bd29ddee285da1b4eda1304ea833a8b
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-01 20:56:19 -0300

    Jogo da velha orientado a objeto commit

--- JavaExercises/src/contabancaria/ContaCorrenteTest.java
+++ JavaExercises/src/contabancaria/ContaCorrenteTest.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
 public class ContaCorrenteTest {
     public static void main(String[] args) {
         
-        ContaCorrente jesus = new ContaCorrente();
+        ContaCorrente conta = new ContaCorrente();
         Scanner scan = new Scanner(System.in);
         
         double valorDeposito;
         double valorSaque;
         
-        jesus.nome = "Jesus";
-        jesus.numeroDaConta = "123456";
-        jesus.agencia = "4321";
-        jesus.especial = true;
-        jesus.limiteEspecial = 500;
-        jesus.saldo = 2000;
+        conta.nome = "Jesus";
+        conta.numeroDaConta = "123456";
+        conta.agencia = "4321";
+        conta.especial = true;
+        conta.limiteEspecial = 500;
+        conta.saldo = 2000;
         
-        System.out.println("Seja bem vindo "+ jesus.nome);
+        System.out.println("Seja bem vindo "+ conta.nome);
         
         boolean exit = false;
         do{
@@ -32,20 +32,20 @@ public class ContaCorrenteTest {
             int opcao = scan.nextInt();
             switch(opcao){
                 case 0 : exit = true; break; // sair 
-                case 1 : jesus.consultarSaldo(); break; // consultar saldo 
+                case 1 : conta.consultarSaldo(); break; // consultar saldo 
                 case 2 : { // depositar
                     System.out.println("Digite o valor do deposito:");
                     valorDeposito = scan.nextDouble();
-                    jesus.depositos(valorDeposito);
+                    conta.depositos(valorDeposito);
                     break;
                 }
                 case 3 : { // sacar 
                     System.out.println("Digite o valor do saque.");
                     valorSaque = scan.nextDouble();
-                    jesus.retirarDinheiro(valorSaque);
+                    conta.retirarDinheiro(valorSaque);
                     break;
                 }
-                case 4 : jesus.verificaUsandoChequeEspecial();break; // consultar cheque especial
+                case 4 : conta.verificaUsandoChequeEspecial();break; // consultar cheque especial
             } 
         } while (!exit);
         
