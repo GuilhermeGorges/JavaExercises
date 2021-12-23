77431f8b46b0d66fb4cb53942a24acbd4357d474
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-12-23 18:55:29 -0300

    Desafio

---
+++ JavaExercises/src/exer/dio/exer15/ColetaOuro.java
@@ -0,0 +1,17 @@
+package exer.dio.exer15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ColetaOuro {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cityNumber = Integer.parseInt(br.readLine());
+        int carriageCapacity = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < cityNumber; i++){
+
+        }
+    }
+}
