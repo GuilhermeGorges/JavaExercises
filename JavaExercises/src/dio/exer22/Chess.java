312a0956bb03d1cf2baf3e1d4ba5c6c72e11db82
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-02-19 16:07:06 -0300

    Java Exercise Chess Final

--- JavaExercises/src/dio/exer22/Chess.java
+++ JavaExercises/src/dio/exer22/Chess.java
@@ -25,8 +25,6 @@ public class Chess {
         Scanner sc = new Scanner(System.in);
 
         int x1,y1,x2,y2;
-        //se estiver na mesma linha ou mesma coluna ou mesma diagonal, gasta 1 movimento
-        //se estiver em qualquer outra posição, a rainha gastará 2 movimentos!
 
         while(true){
             x1 = sc.nextInt();
@@ -38,7 +36,7 @@ public class Chess {
                 System.out.println("0");
             else if(x1 == 0 && y1 == 0 && x2 == 0 && y2 == 0)
                 System.out.println();
-            else if(x1 == x2 || y1 == y2)
+            else if(x1 == x2 || y1 == y2 || (Math.abs(x1-x2) == Math.abs(y1-y2)))
                 System.out.println("1");
             else
                 System.out.println("2");
